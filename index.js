ea271013f8cd50676af4ac98453d75bf2ad95604
Adam <adamlind@live.se>
2022-11-17 13:26:54 +0100

    initial commit with working test

---
+++ index.js
@@ -0,0 +1,29 @@
+const prompt = require('prompt-sync')({sigint: true})
+
+function app () {
+  var users = []
+  var companies = []
+  var running = true
+
+  while (running) {
+    console.log("What would you like to do?")
+    console.log("Press 1 to add a user")
+    console.log("Press 2 to add a company")
+    console.log("Press 3 to exit")
+
+    var choice = prompt()
+    if(choice === "1") {
+      var userName = prompt("Enter a username: ")
+      var userId = 1
+      users.push({userName, userId})
+    } else if(choice === "2") {
+      var companyName = prompt("Enter company name: ")
+      var companyId = 1
+      companies.push({companyName, companyId})
+    } else if(choice === "3") {
+      running = false
+    }
+  }
+}
+
+app()
